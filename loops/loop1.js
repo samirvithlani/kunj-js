38c06dfee1b1199823a133cff16bc07bcd5d36de
samirvithlani <samir.vithlani83955@gmail.com>
2025-03-31 16:29:37 +0530

    loops

---
+++ loops/loop1.js
@@ -0,0 +1,24 @@
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+
+sp = parseInt(prompt("enter starting point"))
+ep = parseInt(prompt("enter ending point"))
+
+var evenSum = 0;
+var oddSum = 0;
+
+for(let i=sp;i<=ep;i++){
+
+    if(i%2==0){
+        console.log(i);
+        evenSum = evenSum + i;
+    }
+    else{
+        oddSum = oddSum + i;
+    }
+
+}
+console.log("Even Sum is: ",evenSum);
+console.log("Odd Sum is: ",oddSum);
---
+++ loops/loop2.js
@@ -0,0 +1,8 @@
+// 5 1*2*3*4*5
+let fact=1;
+for(let i=1;i<=5;i++){
+
+    fact = fact *i;
+    
+}
+console.log(fact)
