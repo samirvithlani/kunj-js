c50bb3b8a4622b03eb4d071a6abdef58b929c15b
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-15 14:08:31 +0530

    question

--- array/jsonarray/jsonArray3.js
+++ array/jsonarray/jsonArray3.js
@@ -44,3 +44,42 @@ var data = {
 //var google = data.companies.find((comp)=>comp.name =="Google").employees.filter((emp)=>emp.job =="Founder").map((emp)=>emp.name)
 //var google = data.companies.find((comp)=>comp.name=="Google").employees.filter((emp)=>emp.salary>150)
 console.log(google)
+
+var data = {
+    states:[
+        {
+            name:"Gujarat",
+            cities:[
+                {
+                    name:"Ahmedabad",
+                    population:32456,
+                    AQI:300
+                },
+                {
+                    name:"Surat",
+                    population:25687,
+                    AQL:280
+                }
+            ]
+        },
+        {
+            name:"Maharashta",
+            cities:[
+                {
+                    name:"MMumbai",
+                    population:45872,
+                    AQI:350
+                },
+                {
+                    name:"Pune",
+                    population:28000,
+                    AQL:290
+                }
+            ]
+        },
+    ]
+}
+
+//find totaal poluation of gujarat
+//find total population of maharashta
+//find all cities of india
